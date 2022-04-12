fb0a299fbc651154244b3368b35f1c7636118de3
Vijay Shinde <vijayns011@gmail.com>
2022-04-12 20:52:25 +0530

    Basic details Save

--- JobPortal/Controllers/EmployeeProfileController.cs
+++ JobPortal/Controllers/EmployeeProfileController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CustomAuthorizationFilter.Infrastructure;
+using CustomAuthorizationFilter.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +8,52 @@ using System.Web.Mvc;
 
 namespace CustomAuthorizationFilter.Controllers
 {
+    [CustomAuthorize("Admin", "SuperAdmin")]
     public class EmployeeProfileController : Controller
     {
+        private SqlDbContext db = new SqlDbContext();
+
         // GET: EmployeeProfile
         public ActionResult Index()
         {
+            if (Session["UserId"] != null)
+            {
+                var userId = Session["UserId"].ToString();
+                var usr = (from user in db.Users where user.UserId == userId select user).ToList<User>();
+                //db.Users.Select(s => s.UserId == Session["UserId"].ToString()).ToList<User>();
+                var UserId = usr[0].Id;
+                List<JobSeeker> jobSeekerLst = (from jbSkr in db.JobSeekers where jbSkr.UserId == UserId select jbSkr).ToList<JobSeeker>();
+                if (jobSeekerLst.Count > 0)
+                {
+                    return View(jobSeekerLst[0]);
+                }
+
+            }
             return View();
+        }
+
+       
+        
+        public ActionResult SaveBasicDetails(JobSeeker jobSeeker)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Session["UserId"] != null)
+                {
+                    var userId = Session["UserId"].ToString();
+                    var usr = (from user in db.Users where user.UserId == userId select user).ToList<User>();
+                    jobSeeker.UserId = usr[0].Id;
+                    jobSeeker.UpdatedDate = DateTime.Now;
+                    jobSeeker.CreatedDate = DateTime.Now;
+
+
+                }
+                    db.JobSeekers.Add(jobSeeker);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(jobSeeker);
         }
     }
 }
--- JobPortal/Models/JobSeeker.cs
+++ JobPortal/Models/JobSeeker.cs
@@ -7,8 +7,12 @@ namespace CustomAuthorizationFilter.Models
         User user { get; set; }
         public int Id { get; set; }
         public string Name { get; set; }
-        public string EmailId { get; set; }
         public string PhoneNumber { get; set; }
+        public string TotalYearsOfExperience { get; set; }
+        public string AnnualSalaryLPA { get; set; }
+        public string AnnualSalaryThousand { get; set; }
+        public string CurrentLocation { get; set; }
+        public int UserId { get; set; }
         public bool IsActive { get; set; }
         public bool IsDeleted { get; set; }
         public DateTime CreatedDate { get; set; }
